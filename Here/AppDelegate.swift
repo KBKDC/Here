91e6fe69e5aaac0ddcbc8a24740f945c3c279762
Yuya Kakui <mail@kakuiyuya.com>
2016-07-19 22:27:12 +0900

    Add Fabric

--- Here/AppDelegate.swift
+++ Here/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 import FBSDKCoreKit
 
 @UIApplicationMain
@@ -16,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        Fabric.with([Crashlytics.self])
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
